0bbc7ff0c7fa9dc4219efcace13680f432b9c2a6
cedric06nice <cedric06nice@mac.com>
2023-04-11 20:15:26 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ScreenShotsMaker
+
+A description of this package.
---
+++ Tests/ScreenShotsMakerTests/ScreenShotsMakerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ScreenShotsMaker
+
+final class ScreenShotsMakerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ScreenShotsMaker().text, "Hello, World!")
+    }
+}
